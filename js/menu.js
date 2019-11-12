4f377ec315c92b9cdeec8fbbdfa7984e7de1c418
Dima <milanac89@yandex.ru>
2019-11-12 03:18:02 +0200

    commit

---
+++ js/menu.js
@@ -0,0 +1,24 @@
+/*мобильное меню*/
+
+	$('.menu img').click(function() {
+	  $('nav').addClass('mobile-nav');
+	  $('.close').show();
+	});
+	$('.close').click(function() {
+        $('nav').removeClass('mobile-nav');
+	  return false;
+	});	
+	
+	$(window).on("resize",function(){
+		if($(window).width() > 768) {
+            $('nav').removeClass('mobile-nav');
+		}
+	});
+
+
+
+
+
+
+
+
